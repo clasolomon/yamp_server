12a103985faae477e245d390eb2e0adb3950a933
ioanclaudiusolomon <claudiusolomon008@gmail.com>
2017-05-13 21:23:20 +0300

    Changed the local-login passport strategy; Changed the register functionality; Updated the /login and /register routes.

--- src/config/passport.js
+++ src/config/passport.js
@@ -3,10 +3,6 @@ import database from '../database';
 import bcrypt from 'bcrypt-nodejs';
 import uuidV4 from 'uuid/v4';
 
-function generateHash(password) {  
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
 function validPassword(password, encrypted) {  
     return bcrypt.compareSync(password, encrypted);
 };
@@ -14,50 +10,22 @@ function validPassword(password, encrypted) {
 function configurePassport(passport) {  
 
     passport.serializeUser(function(user, done) {
-        console.log("serializeUser");
-        done(null, user.email);
+        done(null, user.user_id);
     });
 
     passport.deserializeUser(function(id, done) {
-        console.log("deserializeUser");
-        User.findById(id, function(err, user) {
-            done(err, user);
-        });
+        return database.findUserById(id)
+            .then(
+                function(user) {
+                    return done(null, user);
+                }
+            )
+            .catch(
+                function(err){
+                    return done(err);
+                }
+            );
     });
-
-    passport.use('local-register', new LocalStrategy({
-        usernameField: 'email',
-        passwordField: 'password',
-        passReqToCallback: true,
-    },
-        function(req, email, password, done) {
-            return database.findUserByEmail(email)// check if the email is already in database
-                .then(
-                    function(user) {
-                        if(user){
-                            console.log('user in local-register:', user);
-                            return done(null, {exists: true, email: email}); // return exists: true if the email is already in database
-                        } else {
-                            var newUser = {
-                                user_id: uuidV4(),
-                                user_name: req.body.user_name,
-                                email: email,
-                                password: generateHash(password)
-                            };
-                            return database.addUser(newUser).then(
-                                function(){
-                                    return done(null, newUser);
-                                }
-                            );
-                        }    
-                    })
-                .catch(
-                    function(err){
-                        console.log(err);
-                        return done(err);
-                    }
-                );
-        }));
 
     passport.use('local-login', new LocalStrategy({
         usernameField: 'email',
@@ -65,7 +33,24 @@ function configurePassport(passport) {
         passReqToCallback: true,
     },
         function(req, email, password, done) {
+            return database.findUserByEmail(email)//search for user's email in database
+                .then(
+                    function(user) {
+                        if(!user){// if user's email is not found
+                            return done(null, false);
+                        }
+                        if(!validPassword(password, user.password)){// if password doesn't match
+                            return done(null, false);
+                        }
+                        delete user['password'];
+                        return done(null, user);
+                    })
+                .catch(
+                    function(err){
+                        return done(err);
+                    }
+                );
         }));
-};
+}
 
 module.exports = configurePassport;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import passport from 'passport';
 import database from '../database';
+import bcrypt from 'bcrypt-nodejs';
+import uuidV4 from 'uuid/v4';
+
+function generateHash(password) {  
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
 
 const router = express.Router();
 
@@ -9,30 +15,41 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.post('/', (req, res, next)=>{
-    database.addUser(req.body);
-    res.end('User added successfully!');
-});
-
-/*
-router.post('/register', (req, res, next)=>{
-    passport.authenticate('local-register', function(err, user){
-        res.json(user).end();
-    })(req, res, next);
-});
-
-router.post('/login', (req, res, next)=>{
-    passport.authenticate('local-login', function(err, user){  
-    })(req, res);
-});
-*/
-
-router.post('/register', passport.authenticate('local-register'), (req, res, next)=>{
-    console.log('req.user:', req.user);
-    res.json(req.user).end();
-});
+router.post('/register', registerUser);
 
 router.post('/login', passport.authenticate('local-login'), (req, res, next)=>{
+    res.json(req.user);
 });
+
+
+function registerUser(req, res, next){
+    return database.findUserByEmail(req.body.email)// check if the email is already in database
+        .then(
+            function(user) {
+                if(user){
+                    res.json({isNewUser:false, email: user.email});
+                } else {// if the user is not in the database then create a new user
+                    var newUser = {
+                        user_id: uuidV4(),
+                        user_name: req.body.user_name,
+                        email: req.body.email,
+                        password: generateHash(req.body.password)
+                    };
+                    return database.addUser(newUser).then(
+                        function(){
+                            res.json({isNewUser:true, email: newUser.email});
+                        }
+                    );
+                }    
+            })
+        .catch(
+            function(err){
+                res.sendStatus(500);
+            }
+        );
+}
+
+
+
 
 module.exports = router;
