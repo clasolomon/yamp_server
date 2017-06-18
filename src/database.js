0550339c3ed9f00c85dadca3b62c2aa4ffe83013
ioanclaudiusolomon <claudiusolomon008@gmail.com>
2017-06-18 18:51:26 +0300

    Add REST API for NonMemberInvitations.

--- src/database.js
+++ src/database.js
@@ -564,6 +564,104 @@ function updateNonMemberMeeting(input){
     let statement = `UPDATE NonMemberMeetings SET ${fields.join(', ')} WHERE meetingId='${input.meetingId}'`;
     return sqlite3_wrapper.runPromisified(statement, 'UPDATE NonMemberMeetings');
 }
+//========================================= NonMemberInvitations ===============================================
+
+/**
+ * Create non member meeting invitation.
+ * @param {object} input - contains input data for columns in "Invitations" table
+ * @return {Promise}
+ */
+function createNonMemberInvitation(input){
+    assert(input, 'input must be specified!');
+    assert(input.invitationId, 'input.invitationId must be specified!');
+    assert(input.meetingId , 'input.meetingId must be specified!');
+    assert(input.attendantEmail , 'input.attendantEmail must be specified!');
+    assert(input.acceptedDatesAndTimes , 'input.acceptedDatesAndTimes must be specified!');
+
+
+    let statement = `INSERT INTO NonMemberInvitations (invitationId, meetingId, attendantEmail, acceptedDatesAndTimes) ` +
+        `VALUES ('${input.invitationId}', '${input.meetingId}', '${input.attendantEmail}', '${input.acceptedDatesAndTimes}')`;
+
+    return sqlite3_wrapper.runPromisified(statement, 'INSERT INTO NonMemberInvitations');
+}
+
+/**
+ * Get all non member meeting invitations.
+ * @return {Promise}
+ */
+function getAllNonMemberInvitations(){
+    let statement = `SELECT invitationId, meetingId, attendantEmail, acceptedDatesAndTimes FROM NonMemberInvitations`;
+    return sqlite3_wrapper.allPromisified(statement, 'SELECT FROM NonMemberInvitations');
+}
+
+/**
+ * Get all non member meeting invitations.
+ * @return {Promise}
+ */
+function getNonMemberInvitationsByMeetingId(id){
+    assert(meetingId, 'meetingId must be specified!');
+
+    let statement = `SELECT invitationId, meetingId, attendantEmail, acceptedDatesAndTimes FROM NonMemberInvitations where meetingId='${id}'`;
+
+    return sqlite3_wrapper.allPromisified(statement, 'SELECT FROM NonMemberInvitations');
+}
+
+/**
+ * Delete all non member meeting invitations.
+ * @return {Promise}
+ */
+function deleteAllNonMemberInvitations(){
+    let statement = `DELETE FROM NonMemberInvitations`;
+    return sqlite3_wrapper.runPromisified(statement, 'DELETE FROM NonMemberInvitations');
+}
+
+/**
+ * Get non member meeting invitation by id.
+ * @param {String} id - the id to search by
+ * @return {Promise}
+ */
+function getNonMemberInvitationById(id){
+    assert(id, 'id must be specified!');
+
+    let statement = `SELECT invitationId, meetingId, attendantEmail, acceptedDatesAndTimes FROM NonMemberInvitations WHERE invitationId='${id}'`;
+
+    return sqlite3_wrapper.getPromisified(statement, 'SELECT FROM NonMemberInvitations');
+}
+
+/**
+ * Delete non member meeting invitation.
+ * @param {String} id - the id of the non member meeting invitation to be deleted 
+ * @return {Promise}
+ */
+function deleteNonMemberInvitation(id){
+    let statement = `DELETE FROM NonMemberInvitations WHERE invitationId='${id}'`;
+    return sqlite3_wrapper.runPromisified(statement, 'DELETE FROM NonMemberInvitations');
+}
+
+/**
+ * Update non member meeting invitation.
+ * @param {String} input - contains update non member meeting invitation data
+ * @return {Promise}
+ */
+function updateNonMemberInvitation(input){
+    assert(input, 'input must be specified!');
+    assert(input.invitationId, 'input.invitationId must be specified!');
+
+    let fields = new Array();
+
+    if(input.meetingId){
+        fields[fields.length] = `meetingId='${input.meetingId}'`;
+    }
+    if(input.attendantEmail){
+        fields[fields.length] = `attendantEmail='${input.attendantEmail}'`;
+    }
+    if(input.acceptedDatesAndTimes){
+        fields[fields.length] = `acceptedDatesAndTimes='${input.acceptedDatesAndTimes}'`;
+    }
+
+    let statement = `UPDATE NonMemberInvitations SET ${fields.join(', ')} WHERE invitationId='${input.invitationId}'`;
+    return sqlite3_wrapper.runPromisified(statement, 'UPDATE NonMemberInvitations');
+}
 
 //===============================================================================================================
 
@@ -603,5 +701,14 @@ module.exports = {
     deleteAllNonMemberMeetings,
     getNonMemberMeetingById,
     deleteNonMemberMeeting,
-    updateNonMemberMeeting
+    updateNonMemberMeeting,
+
+    //NonMemberInvitations
+    createNonMemberInvitation,
+    getAllNonMemberInvitations,
+    getNonMemberInvitationsByMeetingId,
+    deleteAllNonMemberInvitations,
+    getNonMemberInvitationById,
+    deleteNonMemberInvitation,
+    updateNonMemberInvitation
 };
