b74a63acc92d146f306af9acf84d561692dadefa
ioanclaudiusolomon <claudiusolomon008@gmail.com>
2017-07-14 18:58:12 +0300

    Organize middleware.

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ function allowCrossDomain(req, res, next) {
 app.set('views', path.join(__dirname, '../public/views'));
 app.set('view engine', 'pug');
 
+// middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,18 +38,19 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(allowCrossDomain);
 
+// passport configuration
 app.use(session({ secret: 'shhsecret' }));
 app.use(passport.initialize());
 app.use(passport.session());
+configurePassport(passport);
 
+// routes
 app.use('/', index);
 app.use('/', users);
 app.use('/', meetings);
 app.use('/', invitations);
 app.use('/', nonMemberMeetings);
 app.use('/', nonMemberInvitations);
-
-configurePassport(passport);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
